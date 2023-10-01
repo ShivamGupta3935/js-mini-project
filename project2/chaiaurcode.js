e44903fd150b8ed82e0d78d8147bb644dcd29118
shivam gupta <guptashivam 3935@gmail.com>
2023-10-01 22:46:57 +0530

    project 2 completed and commited

--- project2/chaiaurcode.js
+++ project2/chaiaurcode.js
@@ -12,6 +12,18 @@ form.addEventListener('submit', function(e){
           results.innerHTML = `please enter a valid weight ${weight}`
       } else{
         const bmi = (weight/((height*height)/10000)).toFixed(3) 
-        results.innerHTML = `The bmi is ${bmi}`
+        results.innerHTML = `<span>${bmi} </span>`
+        let newEle = document.createElement('p')
+        if(bmi < 18.6){
+            newEle.innerHTML = 'under weight'
+            results.appendChild(newEle)
+        }else if(bmi > 18.6 && bmi < 24.9){
+            newEle.innerHTML = 'Normal weight'
+            results.appendChild(newEle)
+        }else{
+            newEle.innerHTML = 'Over weight'
+            results.appendChild(newEle)
+        }
+        
       }
 })
